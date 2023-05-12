067015117a5f767d12b42e88769a91f22923dcb0
IgorKilipenko <igorkili@mail.ru>
2023-05-13 01:24:26 +0700

    start solve sprint-16 / less-8: (Электронная таблица / Ячейки)

---
+++ spreadsheet/cell.cpp
@@ -0,0 +1,18 @@
+#include "cell.h"
+
+#include <cassert>
+#include <iostream>
+#include <optional>
+#include <string>
+
+// Реализуйте следующие методы
+Cell::Cell() {}
+
+Cell::~Cell() {}
+
+void Cell::Set(std::string text) {}
+
+void Cell::Clear() {}
+
+Cell::Value Cell::GetValue() const {}
+std::string Cell::GetText() const {}
---
+++ spreadsheet/cell.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "common.h"
+#include "formula.h"
+
+class Cell : public CellInterface {
+public:
+    Cell();
+    ~Cell();
+
+    void Set(std::string text);
+    void Clear();
+
+    Value GetValue() const override;
+    std::string GetText() const override;
+
+private:
+    // можете воспользоваться нашей подсказкой, но это необязательно.
+    /*    class Impl;
+        class EmptyImpl;
+        class TextImpl;
+        class FormulaImpl;
+        std::unique_ptr<Impl> impl_;
+    */
+};
