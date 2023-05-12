f419f1c43f358e9846460445b36c69feed39cc88
IgorKilipenko <igorkili@mail.ru>
2023-05-13 03:05:51 +0700

    solved sprint-16 / less-8: (Электронная таблица / Ячейки)

--- spreadsheet/cell.cpp
+++ spreadsheet/cell.cpp
@@ -4,15 +4,31 @@
 #include <iostream>
 #include <optional>
 #include <string>
+#include <type_traits>
 
 // Реализуйте следующие методы
-Cell::Cell() {}
+Cell::Cell() : impl_(std::make_unique<EmptyImpl>()) {}
 
 Cell::~Cell() {}
 
-void Cell::Set(std::string text) {}
+void Cell::Set(std::string text) {
+    if (text.empty()) {
+        impl_ = std::make_unique<EmptyImpl>();
+    } else if (text.length() > 1 && text[0] == '=') {
+        impl_ = std::make_unique<FormulaImpl>(std::move(text.erase(0, 1)));
+    } else {
+        impl_ = std::make_unique<TextImpl>(std::move(text));
+    }
+}
 
-void Cell::Clear() {}
+void Cell::Clear() {
+    impl_ = nullptr;
+}
 
-Cell::Value Cell::GetValue() const {}
-std::string Cell::GetText() const {}
+Cell::Value Cell::GetValue() const {
+    return impl_->GetValue();
+}
+
+std::string Cell::GetText() const {
+    return impl_->GetText();
+}
--- spreadsheet/cell.h
+++ spreadsheet/cell.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <algorithm>
+#include <variant>
+
 #include "common.h"
 #include "formula.h"
 
@@ -8,7 +11,7 @@ public:
     Cell();
     ~Cell();
 
-    void Set(std::string text);
+    void Set(std::string text) override;
     void Clear();
 
     Value GetValue() const override;
@@ -16,10 +19,56 @@ public:
 
 private:
     // можете воспользоваться нашей подсказкой, но это необязательно.
-    /*    class Impl;
-        class EmptyImpl;
-        class TextImpl;
-        class FormulaImpl;
-        std::unique_ptr<Impl> impl_;
-    */
+    class Impl {
+    public:
+        virtual ~Impl() = default;
+        virtual CellInterface::Value GetValue() const = 0;
+        virtual std::string GetText() const = 0;
+    };
+
+    class EmptyImpl : public Impl {
+    public:
+        EmptyImpl() = default;
+        CellInterface::Value GetValue() const override {
+            return 0.0;
+        }
+        std::string GetText() const override {
+            return {};
+        }
+    };
+
+    class TextImpl : public Impl {
+    public:
+        explicit TextImpl(std::string text) : text_{std::move(text)} {}
+        CellInterface::Value GetValue() const override {
+            return text_.length() > 0 && text_[0] == '\'' ? text_.substr(1) : text_;
+        }
+        std::string GetText() const override {
+            return text_;
+        }
+
+    private:
+        std::string text_;
+    };
+
+    class FormulaImpl : public Impl {
+    public:
+        explicit FormulaImpl(std::string text) : formula_{ParseFormula(std::move(text))} {}
+        CellInterface::Value GetValue() const override {
+            FormulaInterface::Value val = formula_->Evaluate();
+            if (std::holds_alternative<double>(val)) {
+                return std::get<double>(val);
+            }
+            return std::get<FormulaError>(val);
+        }
+        std::string GetText() const override {
+            return '=' + formula_->GetExpression();
+        }
+
+    private:
+        std::unique_ptr<FormulaInterface> formula_;
+    };
+
+private:
+    std::unique_ptr<Impl> impl_;
 };
