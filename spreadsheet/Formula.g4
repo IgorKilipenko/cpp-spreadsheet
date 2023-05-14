5761d57574568d3d9a8e266645329a355e04c2c7
IgorKilipenko <igorkili@mail.ru>
2023-05-14 23:24:30 +0700

    start dev final proj

--- spreadsheet/Formula.g4
+++ spreadsheet/Formula.g4
@@ -9,9 +9,9 @@ expr
     | (ADD | SUB) expr  # UnaryOp
     | expr (MUL | DIV) expr  # BinaryOp
     | expr (ADD | SUB) expr  # BinaryOp
+    | CELL  # Cell
     | NUMBER  # Literal
     ;
-
 
 // number literals cannot be signed, or else 1-2 would be lexed as [1] [-2]
 fragment INT: [-+]? UINT ;
@@ -26,4 +26,5 @@ ADD: '+' ;
 SUB: '-' ;
 MUL: '*' ;
 DIV: '/' ;
+CELL: [A-Z]+[0-9]+ ;
 WS: [ \t\n\r]+ -> skip ;
--- spreadsheet/common.h
+++ spreadsheet/common.h
@@ -34,12 +34,33 @@ struct Size {
 };
 
 // Описывает ошибки, которые могут возникнуть при вычислении формулы.
-class FormulaError : public std::runtime_error {
+class FormulaError {
 public:
-    using std::runtime_error::runtime_error;
+    enum class Category {
+        Ref,    // ссылка на ячейку с некорректной позицией
+        Value,  // ячейка не может быть трактована как число
+        Div0,   // в результате вычисления возникло деление на ноль
+    };
+
+    FormulaError(Category category);
+
+    Category GetCategory() const;
+
+    bool operator==(FormulaError rhs) const;
+
+    std::string_view ToString() const;
+
+private:
+    Category category_;
 };
 
 std::ostream& operator<<(std::ostream& output, FormulaError fe);
+
+// Исключение, выбрасываемое при попытке передать в метод некорректную позицию
+class InvalidPositionException : public std::out_of_range {
+public:
+    using std::out_of_range::out_of_range;
+};
 
 // Исключение, выбрасываемое при попытке задать синтаксически некорректную
 // формулу
@@ -48,21 +69,12 @@ public:
     using std::runtime_error::runtime_error;
 };
 
-// Исключение, выбрасываемое при попытке передать в метод некорректную позицию
-class InvalidPositionException : public std::out_of_range {
-public:
-    using std::out_of_range::out_of_range;
-};
-
-// Исключение, выбрасываемое, если вставка строк/столбцов в таблицу приведёт к
-// ячейке с позицией больше максимально допустимой
-class TableTooBigException : public std::runtime_error {
+// Исключение, выбрасываемое при попытке задать формулу, которая приводит к
+// циклической зависимости между ячейками
+class CircularDependencyException : public std::runtime_error {
 public:
     using std::runtime_error::runtime_error;
 };
-
-inline constexpr char FORMULA_SIGN = '=';
-inline constexpr char ESCAPE_SIGN = '\'';
 
 class CellInterface {
 public:
@@ -72,15 +84,6 @@ public:
 
     virtual ~CellInterface() = default;
 
-    // Задаёт содержимое ячейки. Если текст начинается со знака "=", то он
-    // интерпретируется как формула. Уточнения по записи формулы:
-    // * Если текст содержит только символ "=" и больше ничего, то он не считается
-    // формулой
-    // * Если текст начинается с символа "'" (апостроф), то при выводе значения
-    // ячейки методом GetValue() он опускается. Можно использовать, если нужно
-    // начать текст со знака "=", но чтобы он не интерпретировался как формула.
-    virtual void Set(std::string text) = 0;
-
     // Возвращает видимое значение ячейки.
     // В случае текстовой ячейки это её текст (без экранирующих символов). В
     // случае формулы - числовое значение формулы или сообщение об ошибке.
@@ -89,14 +92,31 @@ public:
     // редактирование. В случае текстовой ячейки это её текст (возможно,
     // содержащий экранирующие символы). В случае формулы - её выражение.
     virtual std::string GetText() const = 0;
+
+    // Возвращает список ячеек, которые непосредственно задействованы в данной
+    // формуле. Список отсортирован по возрастанию и не содержит повторяющихся
+    // ячеек. В случае текстовой ячейки список пуст.
+    virtual std::vector<Position> GetReferencedCells() const = 0;
 };
+
+inline constexpr char FORMULA_SIGN = '=';
+inline constexpr char ESCAPE_SIGN = '\'';
 
 // Интерфейс таблицы
 class SheetInterface {
 public:
     virtual ~SheetInterface() = default;
 
-    // Задаёт содержимое ячейки.
+    // Задаёт содержимое ячейки. Если текст начинается со знака "=", то он
+    // интерпретируется как формула. Если задаётся синтаксически некорректная
+    // формула, то бросается исключение FormulaException и значение ячейки не
+    // изменяется. Если задаётся формула, которая приводит к циклической
+    // зависимости (в частности, если формула использует текущую ячейку), то
+    // бросается исключение CircularDependencyException и значение ячейки не
+    // изменяется.
+    // Уточнения по записи формулы:
+    // * Если текст содержит только символ "=" и больше ничего, то он не считается
+    // формулой
     // * Если текст начинается с символа "'" (апостроф), то при выводе значения
     // ячейки методом GetValue() он опускается. Можно использовать, если нужно
     // начать текст со знака "=", но чтобы он не интерпретировался как формула.
