359f8f4a36d7e4b3597bc39d2c41045afeb5c5e9
IgorKilipenko <igorkili@mail.ru>
2024-05-14 15:01:49 +0000

    mig to add antlr as ExternalProject

---
+++ spreadsheet/cmake/download_antlr.cmake
@@ -0,0 +1,11 @@
+include(ExternalProject)
+
+ExternalProject_Add(
+    antlr
+    PREFIX ${CMAKE_BINARY_DIR}/antlr
+    URL https://www.antlr.org/download/antlr-4.13.1-complete.jar
+    URL_HASH SHA256=f1b5a2ae0b9876cc3e4fa7c56bb08cbf9b9bbcc0c8c0ed13e4d8b8f7e7c8b47b
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/antlr/src/antlr/antlr-4.13.1-complete.jar ${CMAKE_BINARY_DIR}/antlr/antlr-4.13.1-complete.jar
+)
