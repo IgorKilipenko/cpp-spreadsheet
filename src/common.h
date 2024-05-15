a0f52a28892aea4411bc63afe5569b9f2c0a86d6
IgorKilipenko <igorkili@mail.ru>
2024-05-15 22:58:26 +0700

    add docs

--- src/common.h
+++ src/common.h
@@ -8,7 +8,13 @@
 #include <variant>
 #include <vector>
 
-// Позиция ячейки. Индексация с нуля.
+inline constexpr char FORMULA_SIGN = '=';
+inline constexpr char ESCAPE_SIGN = '\'';
+
+/**
+ * Position represents a position in a 2D space using row and column indices.
+ * Indices are zero-based.
+ */
 struct Position {
     int row = 0;
     int col = 0;
@@ -33,13 +39,15 @@ struct Size {
     bool operator==(Size rhs) const;
 };
 
-// Описывает ошибки, которые могут возникнуть при вычислении формулы.
+/**
+ * Describes errors that can occur when computing a formula.
+ */
 class FormulaError {
 public:
     enum class Category {
-        Ref,    // ссылка на ячейку с некорректной позицией
-        Value,  // ячейка не может быть трактована как число
-        Div0,   // в результате вычисления возникло деление на ноль
+        Ref,            // reference error (cell reference has an invalid position)
+        Value,          // the cell value cannot be interpreted as a number
+        Div0,           // division by zero occurred during computation
     };
 
     FormulaError(Category category);
@@ -56,92 +64,163 @@ private:
 
 std::ostream& operator<<(std::ostream& output, FormulaError fe);
 
-// Исключение, выбрасываемое при попытке передать в метод некорректную позицию
+/**
+ * Exception thrown when an invalid position is passed to a method
+ */
 class InvalidPositionException : public std::out_of_range {
 public:
     using std::out_of_range::out_of_range;
 };
 
-// Исключение, выбрасываемое при попытке задать синтаксически некорректную
-// формулу
+/**
+ * FormulaException is an exception that is thrown when a syntactically
+ * invalid formula is encountered.
+ */
 class FormulaException : public std::runtime_error {
 public:
     using std::runtime_error::runtime_error;
 };
 
-// Исключение, выбрасываемое при попытке задать формулу, которая приводит к
-// циклической зависимости между ячейками
+/**
+ * CyclicDependencyException is an exception that is thrown when a circular
+ * dependency between cells is detected while computing a formula.
+ */
 class CircularDependencyException : public std::runtime_error {
 public:
     using std::runtime_error::runtime_error;
 };
 
+/**
+ * @class CellInterface
+ * @brief An interface representing a cell in a spreadsheet.
+ *
+ * The CellInterface class defines the essential operations that can be performed
+ * on a cell within a spreadsheet, such as retrieving its value, text, and any
+ * referenced cells. Cells can contain text, numeric values, or formulas that may
+ * result in a value or an error.
+ */
 class CellInterface {
 public:
-    // Либо текст ячейки, либо значение формулы, либо сообщение об ошибке из
-    // формулы
     using Value = std::variant<std::string, double, FormulaError>;
 
     virtual ~CellInterface() = default;
 
-    // Возвращает видимое значение ячейки.
-    // В случае текстовой ячейки это её текст (без экранирующих символов). В
-    // случае формулы - числовое значение формулы или сообщение об ошибке.
+    /**
+     * @brief Returns the visible value of the cell.
+     *
+     * For a text cell, this returns the text without any escape characters.
+     * For a formula cell, this returns the numerical value of the formula or an error message.
+     *
+     * @return The visible value of the cell as a std::variant containing either a string,
+     *         a double, or a FormulaError.
+     */
     virtual Value GetValue() const = 0;
-    // Возвращает внутренний текст ячейки, как если бы мы начали её
-    // редактирование. В случае текстовой ячейки это её текст (возможно,
-    // содержащий экранирующие символы). В случае формулы - её выражение.
+
+    /**
+     * @brief Returns the internal text of the cell as if it were being edited.
+     *
+     * For a text cell, this returns the text, which may include escape characters.
+     * For a formula cell, this returns the formula expression.
+     *
+     * @return The internal text of the cell as a std::string.
+     */
     virtual std::string GetText() const = 0;
 
-    // Возвращает список ячеек, которые непосредственно задействованы в данной
-    // формуле. Список отсортирован по возрастанию и не содержит повторяющихся
-    // ячеек. В случае текстовой ячейки список пуст.
+    /**
+     * Returns a list of cells that are directly referenced by this formula.
+     * The list is sorted in ascending order and does not contain duplicate cells.
+     * In the case of a text cell, the list is empty.
+     */
     virtual std::vector<Position> GetReferencedCells() const = 0;
 };
-
-inline constexpr char FORMULA_SIGN = '=';
-inline constexpr char ESCAPE_SIGN = '\'';
 
 // Интерфейс таблицы
 class SheetInterface {
 public:
     virtual ~SheetInterface() = default;
 
-    // Задаёт содержимое ячейки. Если текст начинается со знака "=", то он
-    // интерпретируется как формула. Если задаётся синтаксически некорректная
-    // формула, то бросается исключение FormulaException и значение ячейки не
-    // изменяется. Если задаётся формула, которая приводит к циклической
-    // зависимости (в частности, если формула использует текущую ячейку), то
-    // бросается исключение CircularDependencyException и значение ячейки не
-    // изменяется.
-    // Уточнения по записи формулы:
-    // * Если текст содержит только символ "=" и больше ничего, то он не считается
-    // формулой
-    // * Если текст начинается с символа "'" (апостроф), то при выводе значения
-    // ячейки методом GetValue() он опускается. Можно использовать, если нужно
-    // начать текст со знака "=", но чтобы он не интерпретировался как формула.
+    /**
+     * @brief Sets the content of a cell.
+     *
+     * If the text begins with the "=" sign, it is interpreted as a formula.
+     * If a syntactically incorrect formula is provided, a FormulaException is thrown,
+     * and the cell's value remains unchanged.
+     * If a formula leads to a cyclic dependency (e.g., if the formula references the
+     * current cell), a CircularDependencyException is thrown, and the cell's value
+     * remains unchanged.
+     *
+     * Formula specifics:
+     * - If the text contains only the "=" symbol and nothing else, it is not considered a formula.
+     * - If the text starts with the "'" (apostrophe) symbol, the apostrophe is omitted when
+     *   retrieving the cell's value using the GetValue() method. This can be used if you need
+     *   the text to start with the "=" sign without it being interpreted as a formula.
+     *
+     * @param pos The position of the cell to set.
+     * @param text The text to set in the cell.
+     */
     virtual void SetCell(Position pos, std::string text) = 0;
 
-    // Возвращает значение ячейки.
-    // Если ячейка пуста, может вернуть nullptr.
+    /**
+     * @brief Returns the value of the cell at the given position.
+     *
+     * If the cell is empty, it may return nullptr.
+     *
+     * @param pos The position of the cell to retrieve.
+     * @return A pointer to the cell's interface. If the cell is empty, it may return nullptr.
+     */
     virtual const CellInterface* GetCell(Position pos) const = 0;
+
+    /**
+     * @brief Retrieves a modifiable pointer to the cell at the given position.
+     *
+     * This method allows modification of the cell content at the specified position.
+     * If the cell is empty, it may return nullptr.
+     *
+     * @param pos The position of the cell to retrieve.
+     * @return A pointer to the cell's interface. If the cell is empty, it may return nullptr.
+     */
     virtual CellInterface* GetCell(Position pos) = 0;
 
-    // Очищает ячейку.
-    // Последующий вызов GetCell() для этой ячейки вернёт либо nullptr, либо
-    // объект с пустым текстом.
+    /**
+     * @brief Clears the content of a cell.
+     *
+     * After calling this method, a subsequent call to GetCell() for this cell
+     * will return either nullptr or an object with empty text.
+     *
+     * @param pos The position of the cell to clear.
+     */
     virtual void ClearCell(Position pos) = 0;
 
-    // Вычисляет размер области, которая участвует в печати.
-    // Определяется как ограничивающий прямоугольник всех ячеек с непустым
-    // текстом.
+    /**
+     * @brief Computes the size of the area involved in printing.
+     *
+     * This method determines the bounding rectangle that encompasses all cells
+     * with non-empty text.
+     *
+     * @return The size of the printable area as a Size object.
+     */
     virtual Size GetPrintableSize() const = 0;
 
-    // Выводит всю таблицу в переданный поток. Столбцы разделяются знаком
-    // табуляции. После каждой строки выводится символ перевода строки. Для
-    // преобразования ячеек в строку используются методы GetValue() или GetText()
-    // соответственно. Пустая ячейка представляется пустой строкой в любом случае.
+    /**
+     * @brief Outputs the entire sheet to the provided stream.
+     *
+     * Columns are separated by tab characters, and each row is followed by a newline character.
+     * The cells are converted to strings using the GetValue() or GetText() methods, respectively.
+     * An empty cell is represented as an empty string in any case.
+     *
+     * @param output The output stream to which the sheet will be printed.
+     */
     virtual void PrintValues(std::ostream& output) const = 0;
+
+    /**
+     * @brief Outputs the text content of the entire sheet to the provided stream.
+     *
+     * Columns are separated by tab characters, and each row is followed by a newline character.
+     * The cells are converted to strings using the GetText() method.
+     * An empty cell is represented as an empty string.
+     *
+     * @param output The output stream to which the sheet will be printed.
+     */
     virtual void PrintTexts(std::ostream& output) const = 0;
 };
 
--- src/formula.h
+++ src/formula.h
@@ -5,35 +5,56 @@
 
 #include "common.h"
 
-// Формула, позволяющая вычислять и обновлять арифметическое выражение.
-// Поддерживаемые возможности:
-// * Простые бинарные операции и числа, скобки: 1+2*3, 2.5*(2+3.5/7)
-// * Значения ячеек в качестве переменных: A1+B2*C3
-// Ячейки, указанные в формуле, могут быть как формулами, так и текстом. Если это
-// текст, но он представляет число, тогда его нужно трактовать как число. Пустая
-// ячейка или ячейка с пустым текстом трактуется как число ноль.
+/**
+ * @brief Interface for working with formulas.
+ *
+ * This interface provides the ability to evaluate and update arithmetic expressions,
+ * which may include simple binary operations, numbers, parentheses, and cell references.
+ * Cells referenced in the formula can contain either formulas or text. If a cell contains
+ * text that represents a number, it will be treated as a number. An empty cell
+ * or a cell with an empty text is treated as the number zero.
+ */
 class FormulaInterface {
 public:
     using Value = std::variant<double, FormulaError>;
 
     virtual ~FormulaInterface() = default;
 
-    // Возвращает вычисленное значение формулы для переданного листа либо ошибку.
-    // Если вычисление какой-то из указанных в формуле ячеек приводит к ошибке, то
-    // возвращается именно эта ошибка. Если таких ошибок несколько, возвращается
-    // любая.
+    /**
+     * @brief Evaluates the formula and returns its computed value or an error.
+     *
+     * @param sheet The sheet interface containing the cell values referenced by the formula.
+     * @return The computed value of the formula if all referenced cells are valid, or an error
+     *         if any referenced cell evaluation results in an error. If multiple errors occur,
+     *         any one of them may be returned.
+     */
     virtual Value Evaluate(const SheetInterface& sheet) const = 0;
 
-    // Возвращает выражение, которое описывает формулу.
-    // Не содержит пробелов и лишних скобок.
+    /**
+     * @brief Returns the expression that describes the formula.
+     *
+     * The returned expression does not contain any whitespace or unnecessary parentheses.
+     *
+     * @return The expression representing the formula.
+     */
     virtual std::string GetExpression() const = 0;
 
-    // Возвращает список ячеек, которые непосредственно задействованы в вычислении
-    // формулы. Список отсортирован по возрастанию и не содержит повторяющихся
-    // ячеек.
+    /**
+     * @brief Returns a list of cells that are directly involved in the evaluation of the formula.
+     *
+     * The list is sorted in ascending order and does not contain duplicate cells.
+     *
+     * @return A vector of Position objects representing the cells referenced by the formula.
+     */
     virtual std::vector<Position> GetReferencedCells() const = 0;
 };
 
-// Парсит переданное выражение и возвращает объект формулы.
-// Бросает FormulaException в случае, если формула синтаксически некорректна.
+/**
+ * @brief Parses the given expression and returns a unique_ptr to a FormulaInterface object.
+ *
+ * @throws FormulaException if the formula is syntactically incorrect.
+ *
+ * @param expression The string representation of the formula to parse.
+ * @return A unique_ptr to a FormulaInterface object representing the parsed formula.
+ */
 std::unique_ptr<FormulaInterface> ParseFormula(std::string expression);
