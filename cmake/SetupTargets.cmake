04544372cae2ae3565c939ecdf324c53753d7455
IgorKilipenko <igorkili@mail.ru>
2024-05-16 22:28:32 +0700

    add private headers

--- cmake/SetupTargets.cmake
+++ cmake/SetupTargets.cmake
@@ -8,55 +8,56 @@ file(GLOB_RECURSE sources CONFIGURE_DEPENDS
 # Exclude main.cpp from sources
 list(FILTER sources EXCLUDE REGEX "${PROJECT_SRC_DIR}/main\\.cpp$")
 
+file(GLOB_RECURSE public_headers CONFIGURE_DEPENDS
+    "${PROJECT_PUBLIC_INCLUDE_DIR}/spreadsheet/*.h"
+)
+
+file(GLOB_RECURSE private_headers CONFIGURE_DEPENDS
+    "${PROJECT_PUBLIC_INCLUDE_DIR}/*.h"
+)
+
+list(REMOVE_ITEM private_headers ${public_headers})
+
 # Create the library
 add_library(libspreadsheet ${ANTLR_FormulaParser_CXX_OUTPUTS} ${sources})
 target_link_libraries(libspreadsheet PRIVATE antlr4_static)
 
 # Specify the include directories
 target_include_directories(libspreadsheet PUBLIC
-    $<BUILD_INTERFACE:${PROJECT_PUBLIC_INCLUDE_DIR}>
-    $<INSTALL_INTERFACE:include>
+    $<BUILD_INTERFACE:${PROJECT_PUBLIC_INCLUDE_DIR}/spreadsheet>
+    $<INSTALL_INTERFACE:include/spreadsheet>
     PRIVATE
     "${antlr_SOURCE_DIR}/runtime/Cpp/runtime/src"
     "${ANTLR_FormulaParser_OUTPUT_DIR}"
+    "${PROJECT_PUBLIC_INCLUDE_DIR}"
 )
 
 # Specify the public headers
-#set_target_properties(libspreadsheet PROPERTIES
-#    PUBLIC_HEADER "${PROJECT_PUBLIC_INCLUDE_DIR}/spreadsheet.h"
-#)
+set_target_properties(libspreadsheet PROPERTIES
+    PUBLIC_HEADER "${PROJECT_PUBLIC_INCLUDE_DIR}/spreadsheet/spreadsheet.h"
+)
 
-#install(
-#    FILES
-#    "${BINARY_PUBLIC_INCLUDE_DIR}/spreadsheet.h"
-#    DESTINATION include
-#)
-
-#install(
-#    DIRECTORY
-#    "${PROJECT_PUBLIC_INCLUDE_DIR}"
-#    DESTINATION include
-#)
+install(
+    FILES
+    ${private_headers}
+    DESTINATION include
+)
 
 # Installation rules
 install(
     TARGETS libspreadsheet #antlr4_static
-    #EXPORT libspreadsheet-targets
+    EXPORT libspreadsheet_targets
     ARCHIVE DESTINATION lib
     LIBRARY DESTINATION lib
     RUNTIME DESTINATION bin
-    PUBLIC_HEADER DESTINATION include
+    PUBLIC_HEADER DESTINATION include/spreadsheet
 )
-
-#install(
-#    EXPORT libspreadsheet-targets
-#    FILE libspreadsheet-config.cmake
-#    DESTINATION lib/cmake/libspreadsheet
-#)
 
 # Create executable target
 add_executable(spreadsheet ${PROJECT_SRC_DIR}/main.cpp)
 add_dependencies(spreadsheet libspreadsheet)
 target_link_libraries(spreadsheet PRIVATE libspreadsheet)
 
-#install(TARGETS spreadsheet)
+install(TARGETS spreadsheet
+    DESTINATION app
+)
--- include/spreadsheet.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include "graph.h"
-#include "sheet.h"
