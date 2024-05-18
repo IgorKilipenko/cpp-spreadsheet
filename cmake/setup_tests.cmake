28481d093a62fcce862c46d76e356aeac1689760
IgorKilipenko <igorkili@mail.ru>
2024-05-18 12:21:06 +0700

    back to single dir for tests

--- cmake/setup_tests.cmake
+++ cmake/setup_tests.cmake
@@ -9,16 +9,4 @@ target_include_directories(spreadsheet_tests
     PRIVATE
     $<BUILD_INTERFACE:${PROJECT_PUBLIC_INCLUDE_DIR}/spreadsheet;${doctest_SOURCE_DIR}/doctest>
 )
-add_test(NAME spreadsheet_lib_test COMMAND spreadsheet_tests)
-
-file(GLOB test_directories LIST_DIRECTORIES true CONFIGURE_DEPENDS
-    "${PROJECT_SRC_DIR}/*/tests/"
-    "${PROJECT_SRC_DIR}*/tests/"
-)
-
-foreach(test_dir ${test_directories})
-    if(IS_DIRECTORY ${test_dir})
-        add_subdirectory(${test_dir})
-        message(STATUS "Added test directory: ${test_dir}")
-    endif()
-endforeach()
+add_test(NAME spreadsheet_tests COMMAND spreadsheet_tests)
